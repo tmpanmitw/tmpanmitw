cd1c07e2884f1106f54bfba6c91afc2af65e718c
tmpanmitw <112293743+tmpanmitw@users.noreply.github.com>
2022-09-23 17:47:29 +0900

    Add files via upload

---
+++ homework.java
@@ -0,0 +1,13 @@
+public class homework {
+    public static void main(String[] args){
+        int var1 = 5;
+        int var2 = 2;
+        System.out.println(var1/var2);
+//        System.out.println((var1.getClass().getName());
+        double var3 = var1/var2;
+        System.out.println(var3);
+//        int var4 =
+//        변수명.getClass().getName()
+    }
+
+}
